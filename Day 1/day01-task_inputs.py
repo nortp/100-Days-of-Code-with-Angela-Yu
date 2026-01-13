f3a8bcc9c4aa86a7f7c064da2af7af4db6bb1d33
Norton Pereira <norton.p@hotmail.com>
2026-01-13 15:23:06 -0300

    tarefa 3: função input()

--- Day 1/day01-task_inputs.py
+++ Day 1/day01-task_inputs.py
@@ -1,3 +1,1 @@
-print("Olá, seu nome é ... Qual seu nome, afinal?\n")
-name=input('Digite seu nome: ')
-print('\nAhh seu nome é ' + name + '!' ' prazer em te conhecer!')
+print('Olá ' + input('Qual seu nome? ') + '!')
