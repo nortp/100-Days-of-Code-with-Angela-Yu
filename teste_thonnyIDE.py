928205b12963c6a8fa3ed65dc51bcc6aa6353d77
Norton Pereira <norton.p@hotmail.com>
2026-01-15 20:55:57 -0300

    arquivo de teste na IDE Thonny para ver passo a passo de como cada código roda/funciona

---
+++ teste_thonnyIDE.py
@@ -0,0 +1,3 @@
+#Recomendação de uso da IDE Thonny, para ver o passo a passo de como o código funciona
+#Este arquivo será usado para teste dos códigos nesta IDE
+print('Olá ' + input('Qual seu nome? ') + '!')
