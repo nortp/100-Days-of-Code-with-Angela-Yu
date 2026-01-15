75c91aee76cebcab846c64cda544a67fa5c1cce4
Norton Pereira <norton.p@hotmail.com>
2026-01-15 20:56:33 -0300

    Adicionado linhas de comentário (orientação da Angela)

--- Day 1/day01-task_inputs.py
+++ Day 1/day01-task_inputs.py
@@ -1,1 +1,4 @@
+#Angela orienta utilizar COMENTÁRIOS nos códigos para que possa explicar a si mesmo o que acontece na linha
+#hashtag para linha, três aspas seguidas para mais linhas
+
 print('Olá ' + input('Qual seu nome? ') + '!')
