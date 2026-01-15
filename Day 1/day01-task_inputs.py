251f00f6e2efcb1761e2d6477409706a4e489e43
Norton Pereira <norton.p@hotmail.com>
2026-01-15 20:59:56 -0300

    dica de uso: ctrl + / no windows para comentar linha

--- Day 1/day01-task_inputs.py
+++ Day 1/day01-task_inputs.py
@@ -1,4 +1,5 @@
 #Angela orienta utilizar COMENTÁRIOS nos códigos para que possa explicar a si mesmo o que acontece na linha
 #hashtag para linha, três aspas seguidas para mais linhas
+#'ctrl + /' no windows, comenta a linha que o cursor estiver piscando
 
 print('Olá ' + input('Qual seu nome? ') + '!')
