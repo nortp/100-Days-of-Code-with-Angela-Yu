4df30d533e09b2bc9f2c2aa2f0f9f4d480e30b80
Norton Pereira <norton.p@hotmail.com>
2026-01-23 17:57:20 -0300

    atualizado

--- Day-2/Tasks/task1_datatype.py
+++ Day-2/Tasks/task1_datatype.py
@@ -1,1 +1,12 @@
-len("Hello")
+#para concatenar STRINGS com outros tipos de dados, como Float, é necessário a vírgula para separar os dois valores.
+#para usar quotes/aspas em uma string, tem que usar aspas triplas 'Deste "jeito" aqui', ou "Deste' aqui"
+
+print("Esta é uma STRING")  #sequência de characters/string
+
+print(2, "Este é um INTEGER") #números inteiros
+
+print(3.14, "Este é um FLOAT") #tipo flutuante, ou para números reais (com vírgula)
+
+#Utilizar ponto para um Data Type do tipo Float com String e não a vírgula.
+
+print(True, "Este é um BOOLEAN")  #tipo BOOLEAN (verdadeiro ou falso)
