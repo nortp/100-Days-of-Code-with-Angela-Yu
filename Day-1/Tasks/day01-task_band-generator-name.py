85f695d7153ba3c3919eb1ee619cef1759b553b7
Norton Pereira <norton.p@hotmail.com>
2026-01-22 21:09:16 -0300

    adiciona script resposta do desafio 4 band name generator: gera nome de banda com base na cidade natal e nome do pet

---
+++ Day-1/Tasks/day01-task_band-generator-name.py
@@ -0,0 +1,6 @@
+print("Bem vindo ao Gerador de Nome de Banda")
+
+grewupCity = input('Qual o nome da cidade em que você nasceu?\n')
+petName = input('Qual o nome do seu pet?\n')
+
+print(grewupCity + " " + petName)
