cbc6bdbb095ecc714e637f30c6a878b35dacd51a
Norton Pereira <norton.p@hotmail.com>
2026-01-28 20:46:11 -0300

    o código aplicado task 2

--- Day-2/Tasks/task2_typeerror_checking_conversion.py
+++ Day-2/Tasks/task2_typeerror_checking_conversion.py
@@ -1,1 +1,11 @@
-len(12345)
+#PAUSE 1. Type checking
+print(type("abc"))
+
+#PAUSE 2. Escrever os 4 tipos de dados primitivos
+print(type("String"))  #sequência de characters/string
+print(type(2)) #números inteiros
+print(type(3.14)) #tipo flutuante, ou para números reais (com vírgula)
+print(type(True)) #tipo BOOLEAN (verdadeiro ou falso)
+
+#PAUSE 3. Usar a conversão
+print("Number of letters in your name: " +  str(len(input("Enter your name:\n"))))
