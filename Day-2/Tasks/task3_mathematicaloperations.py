08869a124534bd1bfc3baaceee4cda87fd65a2e2
Norton Pereira <norton.p@hotmail.com>
2026-01-28 21:03:42 -0300

    task 3 dia 2

--- Day-2/Tasks/task3_mathematicaloperations.py
+++ Day-2/Tasks/task3_mathematicaloperations.py
@@ -1,5 +1,5 @@
 #PAUSE 1. Resultado = 10
 print(3 * 3 + 3 / 3 - 3)
 
-#PAUSE 2. Colocar os parênteses no 3 + 3, irá resultar em 3, como solicitado.
+#PAUSE 2. Colocar os parênteses no 3 + 3, irá resultar em 3, como solicitado..
 print(3 * (3 + 3) / 3 - 3)
