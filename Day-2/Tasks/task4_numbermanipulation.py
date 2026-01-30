7a380feaded1e3630ee3a4722149d71b10ae2dd9
Norton Pereira <norton.p@hotmail.com>
2026-01-29 23:05:31 -0300

    Update task4_numbermanipulation.py

--- Day-2/Tasks/task4_numbermanipulation.py
+++ Day-2/Tasks/task4_numbermanipulation.py
@@ -1,1 +1,13 @@
 bmi = 84 / 1.65 ** 2
+
+#rouding a number
+print(bmi)
+print(round(bmi, 2))
+
+#assignment operators
+x = 6
+x += 2
+print(x)
+
+#f-strings
+print(f"O valor da minha variável 'x' é: {x}")
